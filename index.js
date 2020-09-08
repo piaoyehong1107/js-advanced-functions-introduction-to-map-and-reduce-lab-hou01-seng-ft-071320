f7731e6a3abe818e3d6bff1cdc3361d64037f128
Yehong Piao <piaoyehong1107@gmail.com>
2020-09-08 04:43:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function mapToSquare(sourceArray){
   return sourceArray.map(ele=>{return ele*ele})
 }
 function reduceToTotal(sourceArray, startingPoint){
-  sourceArray.reduce( (startingPoint)=> {ele+startingPoint},startingPoint)
+  sourceArray.reduce( (startingPoint,ele)=> {ele+startingPoint},startingPoint)
 }
 function reduceToAllTrue(sourceArray){
   
